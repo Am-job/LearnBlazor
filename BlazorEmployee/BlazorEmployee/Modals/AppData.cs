cb0405f8768cad8bdd3273a4c59fe6c1ddb0b313
Aung Myo <itstaram.mdy@gmail.com>
2023-03-07 17:28:21 +0630

    7323

--- BlazorEmployee/BlazorEmployee/Modals/AppData.cs
+++ BlazorEmployee/BlazorEmployee/Modals/AppData.cs
@@ -6,12 +6,12 @@ namespace BlazorEmployee.Modals
         public int ID { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Age { get; set; }
-        public DateOnly Dob { get; set; }
+        public DateOnly Dob { get; set; }      //3-Jan-1998
 
         public string Position { get; set; } = string.Empty;
 
         public decimal Salary { get; set; }
 
-        public DateTime JoinDate { get; set; }
+        public DateTime JoinDate { get; set; } //3-Jan-1998
     }
 }
---
+++ BlazorEmployee/BlazorEmployee/wwwroot/js/browserResize.js
@@ -0,0 +1,20 @@
+﻿// Manages the registered event listeners so they can be disposed later
+let windowEventListeners = {};
+
+function AddWindowWidthListener(objReference) {
+    let eventListener = () => UpdateWindowWidth(objReference);
+    window.addEventListener("resize", eventListener);
+    windowEventListeners[objReference] = eventListener;
+}
+
+function RemoveWindowWidthListener(objReference) {
+    window.removeEventListener("resize", windowEventListeners[objReference]);
+}
+
+function UpdateWindowWidth(objReference) {
+    objReference.invokeMethodAsync("UpdateWindowWidth", window.innerWidth);
+}
+
+function GetInitialWidthFromJs() {
+    DotNet.invokeMethodAsync("GetInitialWidth", window.innerWidth);
+}
