526500b73e080f95ce620204f28d115ea6f8ad73
Aung Myo <itstaram.mdy@gmail.com>
2023-03-18 17:12:44 +0630

    18323

---
+++ BlazorObjectRefHelper/BlazorObjectRefHelper/Pages/ButtonHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorObjectRefHelper.Pages
+{
+    public class ButtonHelper
+    {
+        public string? Name { get; set; }
+        private readonly IJSRuntime js;
+
+        [JSInvokable]
+        public string GetHelloMessage() => $"Hello, {Name}!";
+
+        public ButtonHelper(IJSRuntime js)
+        {
+            this.js = js;
+        }
+        public ButtonHelper(IJSRuntime js, string name)
+        {
+            this.js = js;
+            Name= name;
+        }
+
+        public async ValueTask<string> CallButtonHelperGetHelloMessage(string name)
+        {
+            using var objRef = DotNetObjectReference.Create(new ButtonHelper(js, name));
+            return await js.InvokeAsync<string>("sayHello1", objRef);
+        }
+    }
+}
+
---
+++ BlazorWindowResizeHelper/BlazorWindowResizeHelper/Pages/MessageUpdateInvokeHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWindowResizeHelper.Pages
+{
+    public class MessageUpdateInvokeHelper
+    {
+        private Action action;
+
+        public MessageUpdateInvokeHelper(Action action)
+        {
+            this.action = action;
+        }
+
+        [JSInvokable]
+        public void UpdateMessageCaller()
+        {
+            action.Invoke();
+        }
+    }
+}
